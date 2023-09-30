09126599c47249d08446ad80b08a51291ff70ebd
gmb sumber <git.gmb@gmail.com>
2023-09-30 12:13:42 +0700

    tambah

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,13 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        User::factory(100)->create();
 
         User::create([
             'name' => 'taufiq',
             'email' => 'taufiqb@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('12345'),
+            'roles' => 'admin',
 
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -44,4 +45,6 @@ Route::get('/', function () {
         return view('pages.app.dashboard-siakad', ['type_menu' => '']);
        
     })->name('home');
+
+    Route::resource('user', UserController::class);
  });
