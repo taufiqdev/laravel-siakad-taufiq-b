a84cd80762c1d50e2168c91713971bf97e784c63
gmb sumber <git.gmb@gmail.com>
2023-10-01 17:54:51 +0700

    tambah file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Subject;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +21,11 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call([UserSeeder::class]);
+        $this->call([
+            UserSeeder::class, 
+            SubjectSeeder::class,
+            ScheduleSeeder::class
+        ]);
+        
     }
 }
